dfa9b404a69d2c501d1a0c95aedaee54d6bcb7dc
Christoph Bensch <ch.bensch@gmx.de>
2020-05-23 18:17:04 +0200

    update

--- extractRawText.py
+++ extractRawText.py
@@ -10,21 +10,15 @@ toSeason = 10
 # Complete text will be stored in "completeCorpus"
 completeCorpus = ''
 
-# Download required stanza packages 
-stanza.download('en', processors='tokenize,pos')
-
-# Setup stanza pipeline
-nlp = stanza.Pipeline('en', processors='tokenize,pos')
-
 # create POS folder if it does not exist
-if not os.path.exists('POS/episodes'):
-    os.makedirs('POS/episodes')
+if not os.path.exists('RAW/'):
+    os.makedirs('RAW/')
 
 for S in range(fromSeason,toSeason+1):
     if(S < 8):
         maxEpisodes = 22
     else:
-        maxEpisodes = 20
+        maxEpisodes = 20 
     
     for E in range(1, maxEpisodes+1):
         if(E <10):
@@ -44,28 +38,13 @@ for S in range(fromSeason,toSeason+1):
         text = soup.find_all(text=True)
         for t in text:
             episodeWithoutTags += '{} '.format(t)
-        
-        # Run pipeline on episode
-        doc = nlp(episodeWithoutTags)
 
         if(E <10):
             savename = str(S)+"-"+"0"+str(E)
         else:
             savename = str(S)+"-"+str(E)
-
-        # output words and write to csv file
-        with open('POS/episodes/' + savename + '.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(["Text", "Upos", "Xpos"])
-            for sentence in doc.sentences:
-                for word in sentence.words:
-                    print("Word:", word.text, " upos:", word.upos, " xpos:", word.xpos)
-                    writer.writerow([word.text, word.upos, word.xpos])
         
-        # completeCorpus += episodeWithoutTags
-
-
-
-
-
+        # Output Episode content without html tags
+        print(episodeWithoutTags,  file=open('RAW/'+savename+'.txt', 'w'))
+        
 
