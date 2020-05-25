d13e93176228168cca15023a0d84a86179fc548f
Christoph Bensch <ch.bensch@gmx.de>
2020-05-25 19:50:52 +0200

    push person wordcloud

---
+++ src/5-Visualizations/wordcloudPersonQuotes.py
@@ -0,0 +1,29 @@
+# Libraries
+import csv
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+srcPath = 'Data/6-Quotes/'
+ 
+dictP = {}
+with open(srcPath + 'ListNames.csv', mode='r') as infile:
+    reader = csv.reader(infile, delimiter=',')
+    line_count = 0
+    for row in reader:
+        # first line is the header
+        if line_count == 0:
+            line_count += 1
+        # others are entities
+        else:
+            dictP[row[0]] = int(row[1])
+
+# print(dictP)
+# Create the wordcloud object
+wordcloud = WordCloud(background_color="white", width=800, height=500, colormap='Blues')
+wordcloud.generate_from_frequencies(frequencies=dictP)
+ 
+# Display the generated image:
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.margins(x=0, y=0)
+plt.show()
