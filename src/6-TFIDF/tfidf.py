00930cb0d683cbfd77d914fa382dd77f3f416005
Christoph Bensch <ch.bensch@gmx.de>
2020-05-26 14:03:16 +0200

    add tfidf

---
+++ src/6-TFIDF/tfidf.py
@@ -0,0 +1,160 @@
+import pandas as pd
+import csv
+import re
+import sys
+import os
+
+from sklearn.feature_extraction import text
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# Set default target person to oneill
+targetPerson = "oneill"
+
+# Get the target person
+if(len(sys.argv) == 2):
+    targetPerson = sys.argv[1]
+else:
+    print("no target person specified! taking oneill")
+
+# WorkDir
+Path = 'Data/7-TFIDF/'
+# SrcDir
+SrcPath = 'Data/6-Quotes/episodes/'
+
+# create WorkDir folder if it does not exist
+if not os.path.exists(Path):
+    os.makedirs(Path)
+
+# Define Seasons
+fromSeason = 1
+toSeason = 10
+
+# regex for names
+regex = re.compile('[^a-zA-Z0-9]')
+
+# All Quotes in dne Series
+dictAllQuotes= [] 
+
+for S in range(fromSeason,toSeason+1):
+    if(S < 8):
+        maxEpisodes = 22
+    else:
+        maxEpisodes = 20
+    
+    # All Quotes of the Season
+    dictSeasonQuotes = [] 
+    for E in range(1, maxEpisodes+1):
+        if(E <10):
+            filename = str(S)+"-"+"0"+str(E)
+        else:
+            filename = str(S)+"-"+str(E)
+
+        # All Quotes of the Episode
+        dictEpisodeQuotes = [] 
+
+        with open(SrcPath + filename + ".csv") as csv_file:
+            # read current CSV file
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            # start with first line 
+            line_count = 0
+            for row in csv_reader:
+                # first line is the header
+                if line_count == 0:
+                    # print(f'Column names are {", ".join(row)}')
+                    line_count += 1
+                # others are entities
+                else:
+                    # everything to lower space
+                    name = row[0].lower()
+                    # remove characters that are not needed
+                    name = regex.sub('', name)
+
+                    if (name  == targetPerson):
+                        quote = row[1].lower()
+                        dictEpisodeQuotes.append(quote)
+
+
+        dictSeasonQuotes.extend(dictEpisodeQuotes)
+
+    dictAllQuotes.extend(dictSeasonQuotes)
+
+def sort_coo(coo_matrix):
+    tuples = zip(coo_matrix.col, coo_matrix.data)
+    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+ 
+    
+def extract_topn_from_vector(feature_names, sorted_items, topn):
+    
+    #use only topn items from vector
+    sorted_items = sorted_items[:topn]
+ 
+    score_vals = []
+    feature_vals = []
+    
+    # word index and corresponding tf-idf score
+    for idx, score in sorted_items:
+        
+        #keep track of feature name and its corresponding score
+        score_vals.append(round(score, 3))
+        feature_vals.append(feature_names[idx])
+ 
+    #create a dictionary of feature,score
+    results= {}
+    for idx in range(len(feature_vals)):
+        results[feature_vals[idx]]=score_vals[idx]
+    
+    return results
+
+def listToString(s):  
+    
+    # initialize an empty string 
+    str1 = ""  
+    
+    # traverse in the string   
+    for ele in s:  
+        str1 += ele   
+    
+    # return string   
+    return str1  
+
+def keyWords(corpus, topN):
+    
+    my_stop_words = text.ENGLISH_STOP_WORDS.union(["don", "ve", "ll", "just", "know", "uh", "did", "didn", "doesn", "going", "like"])
+
+    cv=CountVectorizer(max_df=0.9,stop_words=my_stop_words)
+    #cv=CountVectorizer(max_df=0.85)
+    word_count_vector=cv.fit_transform(corpus)
+
+    tfidf_transformer=TfidfTransformer(smooth_idf=True,use_idf=True)
+    tfidf_transformer.fit(word_count_vector)
+
+    # you only needs to do this once, this is a mapping of index to 
+    feature_names=cv.get_feature_names()
+    
+    #generate tf-idf for the given document
+    tf_idf_vector=tfidf_transformer.transform(cv.transform([listToString(corpus)]))
+    
+    #sort the tf-idf vectors by descending order of scores
+    sorted_items=sort_coo(tf_idf_vector.tocoo())
+    
+    #extract only the top n; n here is 10
+    keywords=extract_topn_from_vector(feature_names,sorted_items,100)
+
+    return keywords
+
+dictP= keyWords(dictAllQuotes, 100)
+
+# Create the wordcloud object
+wordcloud = WordCloud(background_color="black", width=800, height=500, colormap='Blues')
+wordcloud.generate_from_frequencies(frequencies=dictP)
+ 
+# Display the generated image:
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.margins(x=0, y=0)
+plt.show()
